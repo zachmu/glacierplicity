4002cc0a33023596b0a4aac0e06ab84af91be0b8
Zach Musgrave <zach.musgrave@gmail.com>
2013-04-16 16:05:09 -0700

    Since one bucket per directory was a catastrophe (you only get 100 for your account), changed the script to just put "large enough" directories into their own buckets. Gets a reasonable split-up of buckets. Also improved logging and followed convetion for method docs

--- glacierplicity.py
+++ glacierplicity.py
@@ -21,11 +21,11 @@
 # Glacierplicity uses duplicity's s3 backend to leverage its incremental backup 
 # at glacier's much more affordable prices. It has two quirks that you should be
 # aware of:
-# 1) It creates a separate s3 bucket for each directory in the tree to be backed 
-#    up. If you have enough storage volume to care about getting glacier prices 
-#    (and you're on a standard US cable modem), you really don't want any 
-#    individual archive getting much bigger than a few dozen gigs or they become 
-#    very unwieldy.
+# 1) Tries to keep the size of each archive (bucket) under control. Mainly it 
+#    does this by putting any directory larger than some threshold (8GB by 
+#    default) into its own, separate archive. This keeps the number of files in 
+#    each archive reasonably small, since duplicity needs to list them when it 
+#    runs. Having tens of thousands gets problematic.
 # 2) Each bucket is named semi-randomly using the md5 sum of the backed-up path.
 #    Bucket names can't be longer than 60 characters or so, which makes naming 
 #    them directly after the path problematic. The path of the archive is also 
@@ -37,7 +37,7 @@
 #
 # http://blog.epsilontik.de/?page_id=68
 #
-# The main thing it does (other than store each directory in its own bucket) is
+# The main thing it does (other than split things into multiple buckets) is
 # to make sure the .manifest files don't get rotated into glacier. It could be
 # pretty much obsoleted if s3 made their lifecycle rules a bit more expressive.
 
@@ -53,6 +53,7 @@ import time
 from boto.s3.lifecycle import Lifecycle, Transition, Rule
 from boto.s3.tagging import Tag, Tags, TagSet
 import sys
+import math
 
 backup_dirs = ['/back/me/up']
 ignore_directories = ['/ignore/this']
@@ -63,6 +64,7 @@ s3_bucket_prefix = "your-name-here-backup-"
 archive_dir = '/local/path/to/permanent/archive/dir'
 duplicity = "duplicity --num-retries 5 --tempdir /ffp/tmp --no-encryption -v8" 
 log_cmd = " >> /ffp/log/backup.log 2>&1"
+dir_size_threshold = 8 * 1024 * 1024 * 1024 # 8GB
 
 def main():
 
@@ -78,10 +80,11 @@ def main():
 	else:
 		backup(s3)
 
-# Restores the current working directory from s3.
-# This only works if the bucket contents have already 
-# been restored from glacier into s3
 def restore(s3):
+	"""Restores the current working directory from s3.
+	This only works if the bucket contents have already 
+	been restored from glacier into s3
+	"""
 	bucket_name = get_bucket_name(cwd)
 	bucket = setup_bucket(s3, cwd, bucket_name)
 
@@ -93,23 +96,52 @@ def restore(s3):
 	
 	cleanup_bucket(s3, bucket)
 
-# Backs up all the directories specified at the top of the script, 
-# as well as their subdirectories
 def backup(s3):
+	"""Backs up all the directories specified at the top of the script, 
+	as well as their subdirectories
+	"""
 	for dir in backup_dirs:
 		for dirname, dirnames, filenames in os.walk(dir):
 			try:
 				if dirname in ignore_directories:
-					dirnames = []
+					del dirnames[:]
 				else:
 					backup_dir(dirname, dirnames, filenames, s3)
 			except:
+				print sys.exc_info()[0]
 				print "Unexpected error, skipping " + dirname 
 
-# Backs up the directory given to s3
 def backup_dir(dirname, dirnames, filenames, s3):
-	if not filenames:
+	"""Backs up the directory given to s3
+	"""
+
+	print "Examining %s" % dirname
+
+	# figure out which subdirectories are big enough to get their own archives	
+	too_big_directories = []
+	total_size = 0
+	for dir in dirnames:		
+		size = dir_size(os.path.join(dirname, dir))
+		if size > dir_size_threshold:
+			print "Putting %s in its own archive because it is %s" \
+			%(os.path.join(dirname, dir), format_bytesize(size))
+			too_big_directories.append(dir)
+		else:
+			total_size += size
+		
+	# bail out if we would be uploading an empty archive
+	if not filenames and (len(too_big_directories) == len(dirnames)):
 		return
+
+	# add the files into our total size
+	for file in filenames:
+		total_size += os.path.getsize(os.path.join(dirname, file))
+
+	print "Backing up an archive of %s rooted at %s" \
+	%(format_bytesize(total_size) ,dirname)
+
+	# we recurse only on these, not any other directories, via os.walk
+	dirnames[:] = too_big_directories
 
 	bucket_name = get_bucket_name(dirname)
 	
@@ -121,7 +153,8 @@ def backup_dir(dirname, dirnames, filenames, s3):
 
 	cmd = duplicity
 	cmd += " --archive-dir " + bucket_archive_dir
-	# tell duplicity not to recurse on any directories
+
+	# tell duplicity not to recurse on any directories we're handling separately
 	for dir in dirnames:
 		cmd += " --exclude " + pipes.quote(os.path.join(dirname, dir))
 	cmd += " " + pipes.quote(dirname)
@@ -137,14 +170,16 @@ def backup_dir(dirname, dirnames, filenames, s3):
 
 	cleanup_bucket(s3, bucket)
  
-# Gets a unique-ish (and consistent) bucket name from the directory path given
 def get_bucket_name(dirname):
+	"""Gets a unique-ish (and consistent) bucket name from the directory path given
+	"""
 	m = md5.new()
 	m.update(dirname)
 	return s3_bucket_prefix + m.hexdigest()
 
-# Ensures the given bucket exists and prepares it for a duplicity run
 def setup_bucket(s3, dirname, bucket_name):
+	"""Ensures the given bucket exists and prepares it for a duplicity run
+	"""
 	if not s3.lookup(bucket_name):
 		s3.create_bucket(bucket_name)
 		time.sleep(5)
@@ -173,9 +208,11 @@ def setup_bucket(s3, dirname, bucket_name):
 
 	return bucket
 
-# Glacier-proofs the bucket	by renaming the .manifest files to not get moved 
-# to glacier via our lifecycle rule
 def cleanup_bucket(s3, bucket):
+	"""Glacier-proofs the bucket by renaming the .manifest files to not get moved 
+	to glacier via our lifecycle rule
+	"""
+
 	# this isn't proof against eventual consistency, but it helps
 	time.sleep(10)
 
@@ -195,5 +232,50 @@ def cleanup_bucket(s3, bucket):
 	lifecycle.append(rule)
 	bucket.configure_lifecycle(lifecycle)
 
-main()
+# Special thanks to lunaryorn for these snippets
+# https://github.com/lunaryorn/snippets/blob/master/python-misc/du.py
 
+def dir_size(path):
+	"""Returns the total size of all files under the 
+	given path, recursively
+	"""
+	total_size = 0
+	for pathname, dirnames, filenames in os.walk(path):        
+		for file in filenames:
+			total_size += os.path.getsize(os.path.join(pathname, file))
+	return total_size
+
+UNIT_NAMES = ('B', 'KB', 'MB', 'GB', 'TB')
+def format_bytesize(size, precision=1):
+    """Fomats a `size` in bytes as string with a unit attached.
+
+    The unit is one of 'B', 'KB', 'MB', 'GB', and 'TB'.  The number is
+    formatted so that it has the smallest possible unit but not more than 3
+    digits before the decimal point.  Unless it's more then 999 terabytes
+    of course.
+
+    How many digits are placed after the decimal point depends on the
+    `precision` parameter.  If the `size` can be formatted as bytes there's
+    no fractional part at all.
+
+    :raises ValueError: if `size` is negative.
+    """
+    if size < 0:
+        raise ValueError('negative size (%r)' % size)
+
+    # As long as there are more than 3 digits in the integer part of the size
+    # and there is a higher unit, divide `size` by 1024.
+    power = 0
+    while size and math.log10(size) >= 3 and power < len(UNIT_NAMES):
+        power += 1
+        size /= 1024
+
+    # A size given in bytes does not have a fractional part.
+    if power == 0:
+        number_format = '%d'
+    else:
+        number_format = '%%.%df' % precision
+
+    return (number_format + ' %-3s') % (size, UNIT_NAMES[power])
+
+main()
